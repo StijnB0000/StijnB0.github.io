6bbb64e5dc7145bf4ecf3d333f4aae7b6c88ff00
StijnB0000 <stijn.boyce@vlietlandcollege.nl>
2024-09-15 21:14:45 +0200

    titlepage update

--- Javascript/index/Head.js
+++ Javascript/index/Head.js
@@ -1,1 +1,11 @@
-console.log("index.html head.js loaded")
+document.addEventListener('visibilitychange', e=>{
+    if (document.visibilityState === 'visible') {
+        console.log("focus")
+        document.title = "Mijn persoonlijke website"
+  //gebruiker is op de pagina
+   } else {
+    console.log("unfocus")
+            document.title = "kom terug!"
+    //gebruiker zit op een andere pagina
+   }  
+})
--- Javascript/kleurenmodellen/Head.js
+++ Javascript/kleurenmodellen/Head.js
@@ -1,1 +1,11 @@
-document.body.tex
+document.addEventListener('visibilitychange', e=>{
+    if (document.visibilityState === 'visible') {
+        console.log("focus")
+        document.title = "Kleurenmodellen"
+  //gebruiker is op de pagina
+   } else {
+    console.log("unfocus")
+            document.title = "kom terug!"
+    //gebruiker zit op een andere pagina
+   }  
+})
