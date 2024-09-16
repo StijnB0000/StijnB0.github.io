0d1a2f60b48cb92d01b88b254a9719bf3d040589
StijnB0000 <stijn.boyce@vlietlandcollege.nl>
2024-09-16 22:16:22 +0200

    plswerkanderswordikboos

--- Javascript/kleurenmodellen/Body.js
+++ Javascript/kleurenmodellen/Body.js
@@ -5,47 +5,98 @@ hoverlistG = document.getElementById("hoverlistG");
 hoverlistB = document.getElementById("hoverlistB");
 hoverlistGe = document.getElementById("hoverlistGe");
 
+//lijst met aan/uit toggle
+let IsRoodActief = false;
+let IsGroenActief = false;
+let IsBlauwActief = false;
+let IsGeelActief = false;
 
 //als de muis erop gaat:
 
 hoverlistR.addEventListener("mouseover", function(){
     console.log("Hoverlist R is hovered");
-    document.getElementById("clrRedBox").style.backgroundColor = "#c3423f";
+    if (!IsRoodActief) {
+        document.getElementById("clrRedBox").style.backgroundColor = "#c3423f";
+    }
 });
 
 hoverlistG.addEventListener("mouseover", function(){
     console.log("Hoverlist G is hovered");
-    document.getElementById("clrGreenBox").style.backgroundColor = "#4b7f52";
+    if (!IsGroenActief) {
+        document.getElementById("clrGreenBox").style.backgroundColor = "#4b7f52";
+    }
 });
 
 hoverlistB.addEventListener("mouseover", function(){
     console.log("Hoverlist B is hovered");
-    document.getElementById("clrBlueBox").style.backgroundColor = "#3c91e6";
+    if (!IsBlauwActief) {
+        document.getElementById("clrBlueBox").style.backgroundColor = "#3c91e6";
+    }
 });
 
 hoverlistGe.addEventListener("mouseover", function(){
     console.log("Hoverlist Ge is hovered");
-    document.getElementById("clrYellowBox").style.backgroundColor = "#FFFF00";
+    if (!IsGeelActief) {
+        document.getElementById("clrYellowBox").style.backgroundColor = "#FFFF00";
+    }
 });
 
 
 // en nu als de muis eraf gaat:
 
 hoverlistR.addEventListener("mouseleave", function(){
-    document.getElementById("clrRedBox").style.backgroundColor = "#00000000";
+    if (!IsRoodActief) {
+        document.getElementById("clrRedBox").style.backgroundColor = "#00000000";
+    }
+});
+
+
+hoverlistB.addEventListener("mouseleave", function(){
+    if (!IsBlauwActief) {
+        document.getElementById("clrBlueBox").style.backgroundColor = "#00000000";
+    }
 });
 
 hoverlistG.addEventListener("mouseleave", function(){
-    document.getElementById("clrGreenBox").style.backgroundColor = "#00000000";
-});
-
-hoverlistB.addEventListener("mouseleave", function(){
-    document.getElementById("clrBlueBox").style.backgroundColor = "#00000000";
+    if (!IsGroenActief) {
+        document.getElementById("clrGreenBox").style.backgroundColor = "#00000000";
+    }
 });
 
 hoverlistGe.addEventListener("mouseleave", function(){
-    document.getElementById("clrYellowBox").style.backgroundColor = "#00000000";
+    if (!IsGeelActief) {
+        document.getElementById("clrYellowBox").style.backgroundColor = "#00000000";
+    }
 });
+
+
+
+
+
+
+
+
+hoverlistR.addEventListener("click", function(){
+    IsRoodActief = !IsRoodActief
+    document.getElementById("clrRedBox").style.backgroundColor = IsRoodActief ? "#c3423f" : "#00000000";
+});
+
+hoverlistG.addEventListener("click", function(){
+    IsGroenActief = !IsGroenActief
+    document.getElementById("clrGreenBox").style.backgroundColor = IsGroenActief ? "#4b7f52" : "#00000000";
+});
+
+hoverlistB.addEventListener("click", function(){
+    IsBlauwActief = !IsBlauwActief
+    document.getElementById("clrBlueBox").style.backgroundColor = IsBlauwActief ? "#3c91e6" : "#00000000";
+});
+
+hoverlistGe.addEventListener("click", function(){
+    IsGeelActief = !IsGeelActief
+    document.getElementById("clrYellowBox").style.backgroundColor = IsGeelActief ? "#FFFF00" : "#00000000";
+});
+
+
 
 
 
