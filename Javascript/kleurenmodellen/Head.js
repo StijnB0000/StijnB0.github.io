52281b234d0d88bb7cd72e1554e9995e63e3d2c9
StijnB0000 <stijn.boyce@vlietlandcollege.nl>
2024-09-17 11:25:21 +0200

    ; update

--- Javascript/kleurenmodellen/Head.js
+++ Javascript/kleurenmodellen/Head.js
@@ -1,11 +1,11 @@
-document.addEventListener('visibilitychange', e=>{
+document.addEventListener('visibilitychange', e => {
     if (document.visibilityState === 'visible') {
-        console.log("focus")
-        document.title = "Kleurenmodellen"
-  //gebruiker is op de pagina
-   } else {
-    console.log("unfocus")
-            document.title = "kom terug!"
-    //gebruiker zit op een andere pagina
-   }  
+        console.log("focus");
+        document.title = "Kleurenmodellen";
+        //gebruiker is op de pagina
+    } else {
+        console.log("unfocus");
+        document.title = "kom terug!";
+        //gebruiker zit op een andere pagina
+    }
 })
